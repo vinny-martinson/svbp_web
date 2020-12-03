2eaa35e33ede86e42c408b1b337ab2a89d938da5
benjilabonte123 <benjilabonte123@gmail.com>
2020-12-03 10:35:42 -0500

    signup & login fixed from landing page

--- client/src/components/RegisterCard.js
+++ client/src/components/RegisterCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, ThemeProvider } from '@material-ui/core/';
 import { CardActions } from '@material-ui/core/';
@@ -11,7 +11,14 @@ import { createMuiTheme } from '@material-ui/core/';
 import { CssBaseline } from '@material-ui/core/';
 import Header from "./Header"
 
+import compose from 'recompose/compose';
+
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import AirBnBCereal from '../assets/AirbnbCerealExtraBold.ttf'
+import withStyles from '@material-ui/core/styles/withStyles';
+import { registerUser } from '../actions/authActions';
 
 const theme = createMuiTheme({
   typography: {
@@ -25,7 +32,7 @@ const theme = createMuiTheme({
   }
 });
 
-const useStyles = makeStyles({
+const styles = theme => ({
   root: {
     flexGrow: 1,
     width: "100%"
@@ -74,62 +81,134 @@ const useStyles = makeStyles({
   },
 });
 
-export default function RegisterCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+class RegisterCard extends Component {
+  //const classes = useStyles();
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      <div className={classes.root}>
-        <Grid container spacing={0}>
-          <Grid item xs>
-          </Grid>
-          <Grid item xs>
-          </Grid>
-          <Grid item xs>
-            <Card className={classes.rootCard}>
-              <CardContent>
-                <Typography variant="h5" className={classes.title} color="textSecondary" gutterBottom>
-                  New Here?
+
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState(() => ({ [name]: value }));
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, email, password } = this.state;
+    const user = {
+      username,
+      email,
+      password
+    };
+    const { history, createUser } = this.props;
+    console.log(user);
+    createUser(user, history);
+  };
+//   test=(props) =>{
+//     console.log(props);
+//     // console.log(props.password);
+//     console.log("yeet");
+// }
+  render() {
+    const {classes} = this.props;
+    const bull = <span className={classes.bullet}>•</span>;
+
+    // const test=(props) =>{
+    //     console.log(props);
+    //     // console.log(props.password);
+    //     console.log("yeet");
+    // }
+  
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <div className={classes.root}>
+          <Grid container spacing={0}>
+            <Grid item xs>
+            </Grid>
+            <Grid item xs>
+            </Grid>
+            <Grid item xs>
+              <Card className={classes.rootCard}>
+                <CardContent>
+                  <Typography variant="h5" className={classes.title} color="textSecondary" gutterBottom>
+                    New Here?
         </Typography>
-                <Typography variant="h6" className={classes.subtitle}>
-                  Create account!
+                  <Typography variant="h6" className={classes.subtitle}>
+                    Create account!
         </Typography>
-                <InputBase
-                  placeholder="Username"
-                  classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                  }}
-                  inputProps={{ 'aria-label': 'search' }}
-                />
-                <InputBase
-                  placeholder="Email address"
-                  classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                  }}
-                  inputProps={{ 'aria-label': 'search' }}
-                />
-                <InputBase
-                  placeholder="Password"
-                  classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                  }}
-                  inputProps={{ 'aria-label': 'search' }}
-                />
-              </CardContent>
-              <CardActions>
-                <Button size="medium" variant="contained" className={classes.button}>Sign Up</Button>
-              </CardActions>
-            </Card>
+                  <InputBase
+                    placeholder="Username"
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                    value={this.state.username}
+                    name="username"
+                    onChange={this.handleInputChange}
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                  <InputBase
+                    placeholder="Email address"
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                    value={this.state.email}
+                    name="email"
+                    onChange={this.handleInputChange}
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                  <InputBase
+                    placeholder="Password"
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                    value={this.state.password}
+                    name="password"
+                    onChange={this.handleInputChange}
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="medium" variant="contained" onClick={this.handleSubmit} className={classes.button}>Sign Up</Button>
+                </CardActions>
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
-    </ThemeProvider>
+        </div>
+      </ThemeProvider>
 
-  );
+    );
+  }
 }
+
+RegisterCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  createUser: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired
+};
+const mapDispatchToProps = dispatch => ({
+  createUser: (user, history) => dispatch(registerUser(user, history))
+});
+
+export default compose(
+  withStyles(styles),
+  connect(
+    undefined,
+    mapDispatchToProps
+  )
+)(RegisterCard);
